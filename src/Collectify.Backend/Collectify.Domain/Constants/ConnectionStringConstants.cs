c993083409fcffd25c0270af4bc22047ba70581e
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-05-18 17:11:08 +0500

    Install Design package for EntityFramework, define local db connection string

---
+++ src/Collectify.Backend/Collectify.Domain/Constants/ConnectionStringConstants.cs
@@ -0,0 +1,7 @@
+namespace Collectify.Domain;
+
+public static class ConnectionStringConstants
+{
+    public const string LocalDbConnectionString = "DbConnectionString";
+    public const string RemoteDbConnectionString = "POSTGRESQLCONNSTR_DbConnectionString";
+}
