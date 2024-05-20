adf80ce22148ff4413c912da213636e7e76fe426
Ilxom <ilxomkarimjonov22@gmail.com>
2024-05-20 19:06:42 +0500

    Add Search form component

---
+++ src/Collectify.FrontEnd/Collectify/src/common/components/searchForm/SearchForm.vue
@@ -0,0 +1,36 @@
+<template>
+
+<form class="max-w-md mx-auto">
+    <div class="relative">
+        <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" :placeholder="props.placeholder" class="block w-full p-4 text-sm border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        <app-button :text="'Search'" :size="ActionComponentSize.ExtraSmall" class="text-white absolute end-2.5 bottom-2.5"></app-button>
+    </div>
+</form>
+
+
+</template>
+
+<script setup lang="ts">
+
+import { ActionComponentSize } from '@/common/constants/ActionComponentSize';
+import AppButton from '../appButton/AppButton.vue';
+
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true
+    },
+    placeholder: {
+        type: String,
+        default: 'Search'
+    }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+</script>
