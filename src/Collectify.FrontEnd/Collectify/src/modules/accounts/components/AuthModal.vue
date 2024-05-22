160165649d6989853cef39a1bdd58bacfb586df3
Ilxom <ilxomkarimjonov22@gmail.com>
2024-05-22 12:48:31 +0500

    Connect auth modal to API

--- src/Collectify.FrontEnd/Collectify/src/modules/accounts/components/AuthModal.vue
+++ src/Collectify.FrontEnd/Collectify/src/modules/accounts/components/AuthModal.vue
@@ -19,13 +19,15 @@
                 <AppButton class="w-full" :text="showLoginForm ? 'Login' : 'Register'" @click="onSubmit"/>
             </div>
         </div>
-    
     </template>
 </ModalBase>
+
+<message-box ref="messageBox"/>
 
 </template>
 
 <script setup lang="ts">
+
 import ModalBase from '@/common/components/modalBase/ModalBase.vue';
 import { ButtonType } from '@/common/components/appButton/ButtonType';
 import FormInput from '@/common/components/formInput/FormInput.vue';
@@ -33,33 +35,78 @@ import AppButton from '@/common/components/appButton/AppButton.vue';
 import { ref } from 'vue';
 import { SignUpDetails } from '../models/SignUpDetails';
 import { SignInDetails } from '../models/SignInDetails';
-
-const showLoginForm = ref<boolean>(true);
+import { AuthenticationService } from '@/infrastructure/services/auth/AuthenticationService';
+import { MessageType } from '@/common/components/messageBox/MessageType';
+import MessageBox from '@/common/components/messageBox/MessageBox.vue'
 
 const props = defineProps({
     isModalActive: {
         type: Boolean,
         required: true
     }
-})
+});
 
 const emit = defineEmits(['closeModal']);
 
+const messageBox = ref();
+const authenticationService = new AuthenticationService();
+const showLoginForm = ref<boolean>(authenticationService.hasAccessToken());
 const signUpDetails = ref<SignUpDetails>(new SignUpDetails());
 const signInDetails = ref<SignInDetails>(new SignInDetails());
 
 const changeAuthType = () => {
     showLoginForm.value = !showLoginForm.value;
+
+    clearInputs();
 }
 
 const closeModal = () => {
     if (props.isModalActive == true) {
         emit('closeModal');   
+
+        clearInputs();
     }
 }
 
-const onSubmit = () => {
+const onSubmit = async () => {
+    if (showLoginForm.value) {
+        await signInAsync();
+    }
+    else {
+        await signUpAsync();
+    }
+}
+
+const signUpAsync = async () => {
+    const response = await authenticationService.signUpAsync(signUpDetails.value);
     
+    if (response) {
+        messageBox.value.showMessage(response.detail, MessageType.Error);
+    }
+    else {
+        await signInAsync();
+    }
+}
+
+const signInAsync = async () => {
+    signInDetails.value.username = signUpDetails.value.username;
+    signInDetails.value.password = signUpDetails.value.password;
+    
+    const response = await authenticationService.signInAsync(signInDetails.value);
+
+    if (response) {
+        messageBox.value.showMessage(response.detail, MessageType.Error);
+    }
+    else {
+        var result = await authenticationService.setCurrentUser();
+
+        closeModal();
+    }
+}
+
+const clearInputs = () => {
+    signUpDetails.value = new SignUpDetails();
+    signInDetails.value = new SignInDetails();
 }
 
 </script>
