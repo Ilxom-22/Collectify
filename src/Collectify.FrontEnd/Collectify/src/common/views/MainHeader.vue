cf732af7f65523f9210fba71cf1d2f39ce23c386
Ilxom <ilxomkarimjonov22@gmail.com>
2024-05-23 22:24:10 +0500

    Refactor main header and error message displaying support

--- src/Collectify.FrontEnd/Collectify/src/common/views/MainHeader.vue
+++ src/Collectify.FrontEnd/Collectify/src/common/views/MainHeader.vue
@@ -21,7 +21,9 @@
         <div class="w-full h-[1px] mt-2 theme-divider"></div>
 
         <auth-modal :isModalActive="isModalActive" @closeModal="closeModal"></auth-modal>
-
+        <confirmation-modal :isActive="confirmationModalActive" :title="'Are you sure you want to log out?'" @confirmationResult="handleConfirmationResult"/>
+        
+        <message-box ref="messageBox"/>
     </div>
 
     
@@ -34,33 +36,40 @@ import AppButton from '../components/appButton/AppButton.vue';
 import { ButtonType } from '../components/appButton/ButtonType';
 import { ActionComponentSize } from '../constants/ActionComponentSize';
 import SearchForm from '../components/searchForm/SearchForm.vue';
-import { onMounted, ref, watch, computed } from 'vue';
+import { ref, watch, computed } from 'vue';
 import AuthModal from '@/modules/accounts/components/AuthModal.vue';
 import { AuthenticationService } from '@/infrastructure/services/auth/AuthenticationService';
 import { useAccountStore } from '@/infrastructure/stores/AccountStore';
 import DropDown from '../components/dropDown/DropDown.vue';
-import { DropDownItem } from '../components/dropDown/DropDownItem';
-import { DropDownItemType } from '../components/dropDown/DropDownItemType';
 import { Role } from '@/modules/accounts/models/Role';
+import ConfirmationModal from '@/modules/accounts/components/ConfirmationModal.vue';
+import MessageBox from '../components/messageBox/MessageBox.vue';
+import type { ProblemDetails } from '@/infrastructure/apiClients/ProblemDetails';
+import { MessageType } from '../components/messageBox/MessageType';
+import { MessageConstants } from '../constants/MessageConstants';
+import router from '@/infrastructure/router/Router';
 
 const authenticationService = new AuthenticationService();
 const accountStore = useAccountStore();
 const searchValue = ref<string>('');
 const isModalActive = ref<boolean>(false);
+const confirmationModalActive = ref<boolean>(false);
+const messageBox = ref();
 
 const currentUser = computed(() => accountStore.account.user);
 
-const dropDownItems = ref<Array<DropDownItem>>([
-    new DropDownItem('Log Out', DropDownItemType.Action)
-]);
+let dropDownItems = Array<string>();
 
 watch(currentUser, (newValue, oldValue) => {
   if (authenticationService.isLoggedIn()) {
     if (currentUser.value.role === Role.Admin) {
-        dropDownItems.value.unshift(new DropDownItem('Admin Panel', DropDownItemType.Route, '/admin'))
+        dropDownItems = [
+            'Admin Panel',
+            'Log Out', 
+         ]
     }
     else {
-        dropDownItems.value = [ new DropDownItem('Log Out', DropDownItemType.Action) ];
+        dropDownItems = [ 'Log Out' ];
     }
   }
 });
@@ -73,9 +82,37 @@ const closeModal = () => {
     isModalActive.value = false;
 }
 
-const handleActionEvent = (action: string) => {
+const handleActionEvent = async (action: string) => {
     if (action == 'Log Out') {
-        console.log('Log out event occured!');
+        confirmationModalActive.value = true;
+    }
+    else if (action == 'Admin Panel') {
+      await router.push('/admin');
+    }
+}
+
+const handleConfirmationResult = async (confirmationResult: boolean) => {
+    confirmationModalActive.value = false;
+
+    if (confirmationResult) {
+        await logOutAsync();
+    }
+}
+
+const displayErrorMessage = (error: ProblemDetails) => {
+    if (error.isUserFriendly) {
+        messageBox.value.showMessage(error.detail, MessageType.Error);
+    }
+    else {
+        messageBox.value.showMessage(MessageConstants.DefaultErrorMessage, MessageType.Error);
+    }
+}
+
+const logOutAsync = async () => {
+    const logOutResult = await authenticationService.logOutAsync();
+
+    if (logOutResult) {
+        displayErrorMessage(logOutResult);
     }
 }
 
